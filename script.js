61f6cfd3f5d3015291ea333846096741204ca7fe
TOLRX <117536170+TOLRX@users.noreply.github.com>
2023-03-14 16:57:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const fetch = require('node-fetch-commonjs')
 const sncfApiKey = '565bd981-93e3-4ffb-b9a3-539b2afd0fd6';
 
 
+// Function charged of finding each train station in the specified French municipality :
 async function getInseeCode(name, type) {
   const url = `https://geo.api.gouv.fr/communes?nom=${name}&fields=departement,region&format=json&geometry=centre`;
   const response = await fetch(url);
@@ -23,6 +24,7 @@ async function getInseeCode(name, type) {
   return null;
 }
 
+// Function charged of finding each train station in the specified French Department :
 async function getStationsInDepartment(departmentName) {
   const inseeCode = await getInseeCodeDepartment(departmentName);
   if (!inseeCode) {
@@ -43,7 +45,7 @@ async function getStationsInDepartment(departmentName) {
   return null;
 }
 
-// De même mais pour les régions
+// Function charged of finding each train station in the specified French Region :
 async function getStationsInRegion(regionName) {
   const inseeCode = await getInseeCodeRegion(regionName);
   if (!inseeCode) {
